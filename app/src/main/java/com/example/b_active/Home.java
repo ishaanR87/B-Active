8b540ffd1287dfaeab3b546a4aefd3257053b532
irawa <c18459302@mytudublin.ie>
2022-02-10 10:49:21 +0000

    working on step feature

--- app/src/main/java/com/example/b_active/Home.java
+++ app/src/main/java/com/example/b_active/Home.java
@@ -2,7 +2,9 @@ package com.example.b_active;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Home extends AppCompatActivity {
@@ -15,5 +17,13 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         stepCounter = (Button) findViewById(R.id.stepcntr);
+
+        stepCounter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StepCounter.class);
+                startActivity(intent);
+            }
+        });
     }
 }
