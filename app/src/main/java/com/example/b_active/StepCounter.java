f17a5971b8291a75fc03d75949b3af6c8c5e1ddd
irawa <c18459302@mytudublin.ie>
2022-02-10 09:57:37 +0000

    hone page, adding options for different features

--- app/src/main/java/com/example/b_active/StepCounter.java
+++ app/src/main/java/com/example/b_active/StepCounter.java
@@ -3,17 +3,12 @@ package com.example.b_active;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class Home extends AppCompatActivity {
-
-    Button stepCounter;
+public class StepCounter extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        stepCounter = (Button) findViewById(R.id.stepcntr);
+        setContentView(R.layout.activity_step_counter);
     }
 }
