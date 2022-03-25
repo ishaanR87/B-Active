bbec6f0b17ba1c45df6cbbc48c44fcfe37c895b8
irawa <c18459302@mytudublin.ie>
2022-03-25 12:43:22 +0000

    added helpers and step detector files

---
+++ app/src/main/java/com/example/b_active/StepCounter.kt
@@ -0,0 +1,24 @@
+package com.example.b_active
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_step_counter.*
+
+class MainActivity : AppCompatActivity(), StepsCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step_counter)
+
+        val intent = Intent(this, StepDetectorService::class.java)
+        startService(intent)
+
+        StepDetectorService.subscribe.register(this)
+    }
+
+    override fun subscribeSteps(steps: Int) {
+        TV_STEPS.setText(steps.toString())
+        TV_CALORIES.setText(GeneralHelper.getCalories(steps))
+        TV_DISTANCE.setText(GeneralHelper.getDistanceCovered(steps))
+    }
+}
