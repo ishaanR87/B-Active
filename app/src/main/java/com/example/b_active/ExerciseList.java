ad69d2811b038c29539a17a244fb4704fd680742
irawa <c18459302@mytudublin.ie>
2022-03-28 00:05:18 +0100

    different pages for different exercises

---
+++ app/src/main/java/com/example/b_active/ExerciseList.java
@@ -0,0 +1,57 @@
+package com.example.b_active;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ExerciseList extends AppCompatActivity {
+
+    String buttonValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ExerciseList);
+
+        Intent intent = getIntent();
+        buttonValue = intent.getStringExtra("value");
+
+        int intValue = Integer.valueOf(buttonValue);
+
+        switch(intValue){
+
+            case 1:
+                setContentView(R.layout.exercise1);
+                break;
+            case 2:
+                setContentView(R.layout.exercise2);
+                break;
+            case 3:
+                setContentView(R.layout.exercise3);
+                break;
+            case 4:
+                setContentView(R.layout.exercise4);
+                break;
+            case 5:
+                setContentView(R.layout.exercise5);
+                break;
+            case 6:
+                setContentView(R.layout.exercise6);
+                break;
+            case 7:
+                setContentView(R.layout.exercise7);
+                break;
+            case 8:
+                setContentView(R.layout.exercise8);
+                break;
+            case 9:
+                setContentView(R.layout.exercise9);
+                break;
+            case 10:
+                setContentView(R.layout.exercise10);
+                break;
+                
+        }
+    }
+}
--- app/src/main/java/com/example/b_active/Workout.java
+++ app/src/main/java/com/example/b_active/Workout.java
@@ -2,18 +2,37 @@ package com.example.b_active;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class Workout extends AppCompatActivity {
 
-
+    int[] newArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout);
+
+        EditText woedittext = findViewById(R.id.workout);
+
+        newArray = new int[]{
+                R.id.exercise1, R.id.exercise2, R.id.exercise3, R.id.exercise4, R.id.exercise5,
+                R.id.exercise6, R.id.exercise7, R.id.exercise8, R.id.exercise9, R.id.exercise10,
+        };
     }
 
     public void imagebuttonclicked(View view) {
+        for(int i = 0; i< newArray.length; i++){
+            if(view.getId() == newArray[i]) {
+                int value = i + 1;
+                Log.i("First",String.valueOf(value));
+                Intent intent = new Intent(Workout.this, ExerciseList.class);
+                intent.putExtra("value",String.valueOf(value));
+                startActivity(intent);
+            }
+        }
     }
 }
