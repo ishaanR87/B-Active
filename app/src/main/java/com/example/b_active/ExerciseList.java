78ddb7dc2c8440fd5b92e6021b54560dbdd38afc
irawa <c18459302@mytudublin.ie>
2022-03-28 23:36:13 +0100

    adding timer function

--- app/src/main/java/com/example/b_active/ExerciseList.java
+++ app/src/main/java/com/example/b_active/ExerciseList.java
@@ -4,10 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ExerciseList extends AppCompatActivity {
 
     String buttonValue;
+    Button startButton;
+    private CountDownTimer countDownTimer;
+    TextView time;
+    private boolean timeRunning;
+    private long timeLeft;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,5 +64,56 @@ public class ExerciseList extends AppCompatActivity {
                 break;
 
         }
+
+        startButton = findViewById(R.id.starttimerex1);
+        time = findViewById(R.id.time);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(timeRunning)
+
+                {
+
+
+                }
+
+                else
+                {
+
+                }
+            }
+        });
+    }
+
+    private void stopTimer()
+    {
+        countDownTimer.cancel();
+        timeRunning=false;
+        startButton.setText("Start");
+    }
+
+    private void startTimer()
+    {
+        final CharSequence value = time.getText();
+        String num1 = value.toString();
+        String num2 = num1.substring(0,2);
+        String num3 = num1.substring(3,5);
+
+        final int number = Integer.valueOf(num2) * 60 + Integer.valueOf(num3);
+        timeLeft = number * 1000;
+
+        countDownTimer = new CountDownTimer(timeLeft,1000) {
+            @Override
+            public void onTick(long millisecondsUntilFinish) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }
+
     }
 }
