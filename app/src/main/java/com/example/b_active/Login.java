d44299706d17701dc5aed73834e912951e6fa63c
irawa <c18459302@mytudublin.ie>
2022-03-29 17:33:13 +0100

    working on login

--- app/src/main/java/com/example/b_active/Login.java
+++ app/src/main/java/com/example/b_active/Login.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
     private TextView register;
+    private EditText etfullname, etemail, etpassword;
+    private Button signIN;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,15 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
         register = (TextView) findViewById(R.id.register);
         register.setOnClickListener(this);
+
+        signIN = (Button) findViewById(R.id.loginbtn);
+        signIN.setOnClickListener(this);
+
+        etemail = (EditText) findViewById(R.id.Emaillogin);
+        etemail.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setOnClickListener(this);
     }
 
     public void onClick(View v)
@@ -29,6 +46,11 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
             case R.id.register:
                 startActivity(new Intent(this, Register.class));
                 break;
+
+            case R.id.loginbtn:
+                startActivity(new Intent(this, Register.class));
+                break;
+
         }
     }
 }
