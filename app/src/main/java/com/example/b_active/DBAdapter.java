39452257e9a523d76641c7f29402996f873807a3
irawa <c18459302@mytudublin.ie>
2022-04-12 04:31:39 +0100

    updated db to clear database

--- app/src/main/java/com/example/b_active/DBAdapter.java
+++ app/src/main/java/com/example/b_active/DBAdapter.java
@@ -14,7 +14,7 @@ public class DBAdapter {
 
     /* 01 Variables ---------------------------------------- */
     private static final String databaseName = "bactive";
-    private static final int databaseVersion = 95;
+    private static final int databaseVersion = 96;
 
     /* 02 Database variables ------------------------------- */
     private final Context context;
