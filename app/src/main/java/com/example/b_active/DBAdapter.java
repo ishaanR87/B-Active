100477c515f81eefe61fc23572dae5b2a918b41e
Ishaan Rawat <i.rawat98@gmail.com>
2022-10-06 17:56:54 +0100

    Added images for readme

--- app/src/main/java/com/example/b_active/DBAdapter.java
+++ app/src/main/java/com/example/b_active/DBAdapter.java
@@ -14,7 +14,7 @@ public class DBAdapter {
 
     /* 01 Variables ---------------------------------------- */
     private static final String databaseName = "bactive";
-    private static final int databaseVersion = 104;
+    private static final int databaseVersion = 105;
 
     /* 02 Database variables ------------------------------- */
     private final Context context;
